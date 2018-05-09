49399b6e77131793298c2d548095bda6f6324b38
johnson19750717 <johnson.he@mobiledaily.cn>
2018-05-10 00:47:04 +0800

    change the database configuration for new machine

--- src/main/java/com/h1infotech/smarthive/web/SmartHiveController.java
+++ src/main/java/com/h1infotech/smarthive/web/SmartHiveController.java
@@ -2,6 +2,7 @@ package com.h1infotech.smarthive.web;
 
 import com.h1infotech.smarthive.domain.Partner;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +15,23 @@ public class SmartHiveController {
     public List<Partner> getPartners() {
         return null;
     }
+
+    @PostMapping(path = "/login")
+    @ResponseBody
+    public List<Partner> login() {
+        return null;
+    }
+
+    @PostMapping(path = "/register")
+    @ResponseBody
+    public List<Partner> register() {
+        return null;
+    }
+
+    @PostMapping(path = "/logout")
+    @ResponseBody
+    public List<Partner> logout() {
+        return null;
+    }
+
 }
