0c5b8682eb41d646345283e0c69cf837236ae8cd
Chris <Chris@172.20.10.9>
2018-05-13 13:53:31 +0800

    BeeBox

---
+++ src/main/java/com/h1infotech/smarthive/repository/BeeBoxRepository.java
@@ -0,0 +1,8 @@
+package com.h1infotech.smarthive.repository;
+
+import com.h1infotech.smarthive.domain.BeeBox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeeBoxRepository extends JpaRepository<BeeBox, Long>{
+
+}
